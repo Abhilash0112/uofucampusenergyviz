dadf9a6e31202071ffd7112c761b2250d1efb414
Kevin Le <infernapeman@gmail.com>
2018-11-02 20:45:00 -0600

    Started on AreaChart basics. Need to figure out Date Parsing.

---
+++ Data_Viz_Project/js/AreaChart.js
@@ -0,0 +1,41 @@
+class AreaChart {
+
+    constructor() {
+        //TODO: Create SVG Bounds
+        this.rawToDate = d3.timeParse("t:%Y-%m-%dT%H:%M:%S%Z Denver");
+        this.dateToTime = d3.timeFormat("%B %d, %Y, %I:%M %p");
+    }
+
+    update() {
+        //The below is a fixed example for now.
+        //Must eventually add a param to update() containing the building(s) to render in the stacked area chart.
+        d3.json("data/JSON_Files/BROWNING_BLDG.JSON").then(bldgData => {
+            console.log(bldgData);
+            console.log(bldgData["cols"][1]["unit"]);
+            let unit = bldgData["cols"][1]["unit"];
+
+            let modifier = 0;
+            if (unit === "kBTU") {
+                modifier = 3.412
+            }
+            else if (unit === "BTU") {
+                modifier = 3.412 / 1000;
+            }
+            else if (unit === "Wh") {
+                modifier = 1000;
+            }
+
+            let that = this;
+
+            let dataArr = bldgData["rows"].map(function (d) {
+                let dataTime = d["ts"];
+                let tempDate = that.rawToDate(dataTime);
+                //TODO: Figure out why this is returning a prototype for some reason.
+                let time = that.dateToTime(tempDate);
+                return time;
+            });
+
+            console.log(dataArr);
+        });
+    }
+}
---
+++ Data_Viz_Project/js/script.js
@@ -0,0 +1,3 @@
+let areaChart = new AreaChart();
+
+areaChart.update();
