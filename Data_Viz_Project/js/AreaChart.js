6daf77b4cc568059cd26456eda0fff22d2411c4c
Kevin Le <infernapeman@gmail.com>
2018-11-18 14:55:35 -0700

    Preparing for merge

--- Data_Viz_Project/js/AreaChart.js
+++ Data_Viz_Project/js/AreaChart.js
@@ -12,7 +12,10 @@ class AreaChart {
         this.x = d3.scaleTime().range([0, this.width]);
         this.y = d3.scaleLinear().range([this.height, 0]);
 
-        this.line = d3.area().x(function (d) { return that.x(d.time) }).y0(function (d) { return that.y(0) }).y1(function (d) { return that.y(d.kWh) });
+        this.line = d3.area()
+            .x(function (d) { return that.x(d.time) })
+            .y0(that.y(0))
+            .y1(function (d) { return that.y(d.kBTU) });
 
         this.svg = d3.select("#chart")
             .append("svg").attr("id", "chartSVG");
@@ -38,50 +41,27 @@ class AreaChart {
         //Making this a stack is gonna be a pain.
         //I might have to use await for all of the selected CSVs to load their data
         //then put them into the stack.
-        //Additional note: After research, I think I've confirmed that I cannot just simply iterate through all of the files
-        //with pure Javascript (dammit Javascript)
-        //I'll need a direct string-to-file to access the correct file in the first place... and that's just all kinds of spaghetti.
-        //I'll have to ask a TA or something to figure this out.
+        
 
-        d3.json("data/JSON_Files/SUTTON_BLDG_208V_PWR.JSON").then(bldgData => {
-            let unit = bldgData["cols"][1]["unit"];
-
-            //Note that this code converts from any unit to kBTU
-            if (unit === "kBTU") {
-                this.modifier = 1.0
-            }
-            else if (unit === "BTU") {
-                this.modifier = 1000.0;
-            }
-            else if (unit === "Wh") {
-                this.modifier = 1000.0 / 3.41214;
-            }
-            else if (unit === "kWh") {
-                this.modifier = 1.0 / 3.41214;
-            }
-
+        d3.csv("data/fixed/combined_csvs/0011.csv").then(bldgData => {
+            console.log(bldgData);
             let that = this;
-            let dataArr = bldgData["rows"].map(function (d) {
+            let dataArr = bldgData.map(function (d) {
                 let formatData = {};
                 let dataTime = d["ts"];
-                let dataEnergy = d["v0"];
 
                 dataTime = dataTime.replace("-06:00 Denver", "");
-                dataTime = dataTime.replace("t:", "");
                 formatData.time = new Date(dataTime);
 
-                dataEnergy = dataEnergy.replace("n:", "");
-                dataEnergy = dataEnergy.replace(" kWh", "");
-                dataEnergy = dataEnergy.replace(" BTU", "");
-                dataEnergy = dataEnergy.replace(" kBTU", "");
-                dataEnergy = dataEnergy.replace(" Wh", "");
+                formatData.kBTU = d["v0"].replace(" kBTU", "");
 
-                formatData.kWh = parseFloat(dataEnergy) / that.modifier;
                 return formatData;
             });
 
+            console.log(dataArr);
+
             this.x.domain(d3.extent(dataArr, function (d) { return d.time }));
-            this.y.domain([0, 100]);
+            this.y.domain([0, 100000]);
             let lineFromData = this.line(dataArr);
 
             this.chartGroup.append("path")
