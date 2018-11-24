f965752cbea456fe6771034c910e7ecd8ce4e8c0
Venkat Abhilash <abhilash@Venkats-MacBook-Pro.local>
2018-11-24 13:13:28 -0700

    created circle sizes based on a variable

---
+++ Data_Viz_Project/js/barChart.js
@@ -0,0 +1,76 @@
+/*class BarChart {
+
+    constructor() {
+
+    }
+
+    update() {
+        d3.csv("/data/CSV_FULL_SITE_LIST.csv").then(bardata =>{
+        console.log(bardata);
+
+        let data = bardata.filter(function(d) { return d.tz == 'Denver';});
+//        console.log(data);
+//        console.log(Object.keys(bardata[0]));
+//        let columns = Object.keys(data[0]);
+
+        let svg = d3.select("body").append("svg")
+                    .attr("height", 1)
+                    .attr("width", 1);
+
+        let table = d3.select("#table-location")
+                      .append("table")
+                      .attr("class", "table table-condensed table-striped"),
+                      thead = table.append("thead"),
+                      tbody = table.append("tbody");
+        let col = bardata.map(function(d){ return { Category: d.mgntGroup, Area: d.area, PrimaryUsage: d.primaryFunction, Type: d.dis};});;
+        console.log(col);
+        let ByName = d3.nest().key(function(d){ return d.Category;}).entries(col);
+        console.log(ByName);
+        let columns = Object.keys(col[0]);
+        let sorting = true;
+        let header = thead.append("tr")
+                          .selectAll("th")
+                          .data(columns)
+                          .enter()
+                          .append("th")
+                          .text(function(columns) { return columns;})
+                          .on("click", function(d){
+                            header.attr('class', 'header');
+                            if(sorting) {
+                                rows.sort(function(a, b){ return b[d] < a[d];});
+                                sorting = false;
+                                this.className = 'aes';
+                            }  else {
+                                rows.sort(function(a, b){ return b[d] > a[d];});
+                                sorting = true;
+                                this.className = 'des';
+                              }
+
+                          });
+
+        let rows = tbody.selectAll("tr")
+                        .data(col)
+                        .enter()
+                        .append("tr")
+                        .on("mouseover", function(d){
+                            d3.select(this).style("background-color", "orange");
+                        })
+                        .on("mouseout", function(d){
+                            d3.select(this).style("background-color", "transparent")
+                        });
+        rows.selectAll("td")
+            .data(function(d){
+                return columns.map(function(k){
+                    return { 'value': d[k], 'name': k}
+                });
+            })
+            .enter()
+            .append("td")
+            .text(function(d){ return d.value;});
+        });
+
+
+    }
+
+
+}*/
--- Data_Viz_Project/js/script.js
+++ Data_Viz_Project/js/script.js
@@ -1,5 +1,8 @@
 let mapChart = new MapChart();
 mapChart.update();
+
+let barChart = new BarChart();
+barChart.update();
 let areaChart = new AreaChart();
 
 areaChart.update(["0011"]);
